f16886497a39c698685adb2719e9e94f04b5adeb
nx <aaa@qq.com>
2019-11-04 16:19:30 +0800

    修改UserController

--- demo1/src/main/java/cn/it/controller/UserController.java
+++ demo1/src/main/java/cn/it/controller/UserController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/user")
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/get")
